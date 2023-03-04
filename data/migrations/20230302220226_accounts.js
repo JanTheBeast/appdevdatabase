2979ea83e8fe23bcc41d1a4250812b142c336814
JanTheBeast <44895646+JanTheBeast@users.noreply.github.com>
2023-03-04 15:59:22 +0100

    actually made account names unique

--- data/migrations/20230302220226_accounts.js
+++ data/migrations/20230302220226_accounts.js
@@ -2,8 +2,9 @@ exports.up = function(knex) {
     return knex.schema.createTable("accounts", function(accounts) {
         accounts.increments();
 
-        accounts.string("name", 128).notNullable().unique();
+        accounts.string("name", 128).notNullable();
         accounts.string("password", 128).notNullable();
+        accounts.unique(["name"]);
     });
 };
 
