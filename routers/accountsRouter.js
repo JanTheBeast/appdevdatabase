355ec6a0c3b287fb9d1185a9f660bf0bc472025f
JanTheBeast <44895646+JanTheBeast@users.noreply.github.com>
2023-03-18 19:00:59 +0100

    added login and create account

--- routers/accountsRouter.js
+++ routers/accountsRouter.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const accountsDB = require("../data/helpers/accountModel")
 
@@ -9,63 +11,125 @@ const router = express.Router()
 router.use(express.json())
 
 router.get("/", auth, (req, res) => {
-  accountsDB.get()
-    .then(accounts => {
-      res.status(200).json(accounts)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong getting your accounts", err })
-    })
+    accountsDB.get()
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong getting your accounts", err })
+        })
 })
 
 router.post("/", auth, (req, res) => {
-  accountsDB.insert(req.body)
-    .then(accounts => {
-      res.status(200).json(accounts)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong adding your account", err })
-    })
+    accountsDB.insert(req.body)
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong adding your account", err })
+        })
+})
+
+router.post('/create', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(500).json({ message: "Empty field found", err })
+        }
+
+        const hash_password = await bcryptjs.hash(password, 12);
+        let users;
+
+        users = await accountsDB.insert({
+            name: name, email: email, password: hash_password
+        })
+
+        const user_id = { user_id: users };
+        const token = jwt.sign(user_id, "key");
+
+        if (!users) {
+            return res.status(500).json({ message: "something went wrong creating your account", err })
+        }
+
+        return res.send({
+            success: true,
+            token: token,
+            message: 'Account create successfully'
+        })
+
+    } catch (err) {
+        return res.status(500).json({ message: "something went wrong creating your account", err })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const { name, password } = req.body;
+        if (!name || !password) {
+            return res.status(500).json({ message: "Empty field found" })
+        }
+        
+        const users = await accountsDB.getByName(name).first();
+        if (!users) {
+            return res.status(500).json({ message: "Invalid name" })
+        }
+        
+        const compare_password = await bcryptjs.compare(password, users.password);
+        if (compare_password) {
+            const user_id = { user_id: users.id };
+            const token = jwt.sign(user_id, "key");
+            return res.send({
+                success: true,
+                token,
+                message: 'Account login successfully'
+            })
+        } else {
+            return res.status(500).json({ message: "Invalid name and password", err })
+        }
+
+    } catch (err) {
+        return res.status(500).json({ message: "something went wrong logging in your account", err })
+    }
 })
 
 router.get("/:id/", auth, (req, res) => {
-  accountsDB.get(req.params.id)
-    .then(account => {
-      res.status(200).json(account)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong getting your account", err })
-    })
+    accountsDB.get(req.params.id)
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong getting your account", err })
+        })
 })
 
 router.get("/name/:name/", auth, (req, res) => {
-  accountsDB.getByName(req.params.name)
-    .then(account => {
-      res.status(200).json(account)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong getting your account", err })
-    })
+    accountsDB.getByName(req.params.name)
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong getting your account", err })
+        })
 })
 
 router.put("/:id", auth, (req, res) => {
-  accountsDB.update(req.params.id, req.body)
-    .then(account => {
-      res.status(200).json(account)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong updating your account", err })
-    })
+    accountsDB.update(req.params.id, req.body)
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong updating your account", err })
+        })
 })
 
 router.delete("/:id", auth, (req, res) => {
-  accountsDB.remove(req.params.id, req.body)
-    .then(account => {
-      res.status(200).json(account)
-    })
-    .catch(err => {
-      res.status(500).json({ message: "something went wrong deleting your account", err })
-    })
+    accountsDB.remove(req.params.id, req.body)
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "something went wrong deleting your account", err })
+        })
 })
 
 module.exports = router
